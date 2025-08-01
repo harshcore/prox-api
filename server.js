d8179a8196f33e0b93200f333b3c5d1bdf880a33
harshcore <153198322+harshcore@users.noreply.github.com>
2025-08-01 21:24:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const { default: axios } = require("axios");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 const { v4: uuidv4 } = require("uuid");
 
 const dummyData = require("./dummy_data.json");
